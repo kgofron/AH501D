ebfb4afc9ac96f0925564c9ca623c32d025f3733
Kaz Gofron <kgofron@bnl.gov>
2021-06-11 16:08:52 -0400

    Debian 10 migration

--- st.cmd
+++ st.cmd
@@ -1,1 +1,22 @@
-/epics/support2/quadEM/iocBoot/iocAH501/st.cmd
+#!/epics/src/Deb9/support/quadEM/bin/linux-x86_64/quadEMTestApp
+
+errlogInit(5000)
+#epicsEnvSet("QUADEM",	"/epics/support2/quadEM")
+epicsEnvSet("QUADEM",	"/epics/src/Deb9/support/quadEM")
+epicsEnvSet("QUAD_DET", "NSLS_EM.cmd")
+epicsEnvSet("IOC",	"iocAH501")
+< $(QUADEM)/iocBoot/iocAH501/envPaths
+< unique.cmd
+
+# Tell EPICS all about the record types, device-support modules, drivers,
+# etc. in this build
+dbLoadDatabase("$(QUADEM)/dbd/quadEMTestApp.dbd")
+quadEMTestApp_registerRecordDeviceDriver(pdbbase)
+
+# The search path for database files
+# Note: the separator between the path entries needs to be changed to a semicolon (;) on Windows
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db:$(QUADEM)/db")
+
+< $(QUADEM)/iocBoot/iocAH501/AH501.cmd
+dbl > ./records.dbl
+< /epics/common/xf10idd-ioc3-netsetup.cmd
--- unique.cmd
+++ unique.cmd
@@ -5,19 +5,17 @@
 #epicsEnvSet("MODEL",     "AH501D")
 
 epicsEnvSet("PREFIX",    "XF10ID-BI:")
-epicsEnvSet("RECORD",    "AH171")
+epicsEnvSet("RECORD",    "AH171:")
 epicsEnvSet("PORT",      "AH171")
 epicsEnvSet("TEMPLATE",  "AH501")
 epicsEnvSet("MODEL",     "AH501D")
 epicsEnvSet("QSIZE",     "20")
 epicsEnvSet("RING_SIZE", "10000")
 epicsEnvSet("TSPOINTS",  "1000")
-# epicsEnvSet("IP",        "10.10.2.171:10001")
+# epicsEnvSet("IP",        "xf10id-ah501d1.nsls2.bnl.local:10001")
 epicsEnvSet("IP",        "10.66.74.171:10001")
 epicsEnvSet("QUAD_DET",   "AH501.cmd")
 
-epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
-epicsEnvSet("EPICS_CA_ADDR_LIST", "10.10.0.255")
 
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "1000000")
 
